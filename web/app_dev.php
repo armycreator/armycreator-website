e6e498b3324a7e71d99a0490a39771c5d6a2822d
j_deniau <julien.deniau@gmail.com>
2012-07-23 09:23:46 +0200

    modifs

--- web/app_dev.php
+++ web/app_dev.php
@@ -4,7 +4,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-//umask(0000);
+umask(0000);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
@@ -12,6 +12,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
+		'195.88.195.146',
         '::1',
     ))
 ) {
