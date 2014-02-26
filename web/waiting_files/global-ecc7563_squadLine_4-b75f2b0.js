5164cc65d43eb27a146a1e86a60ac513ff502e5f
Julien Deniau <julien.deniau@gmail.com>
2014-02-26 18:48:00 +0100

    waiting page

---
+++ web/waiting_files/global-ecc7563_squadLine_4-b75f2b0.js
@@ -0,0 +1,89 @@
+$(document).on('click', '#otherTypeToggle', function() {
+    $('.otherType').toggle('fast');
+});
+
+$(document).on('click', '#otherBreedToggle', function() {
+    $('.otherBreed').toggle('fast');
+});
+
+$(document).ready(function() {
+    if ($('#websiteContent').attr('class').match(/^squad_/)) {
+        $('.asManyAsUnit')
+            .on('change', 'input[type="checkbox"]', function () {
+                if ($(this).is(':checked')) {
+                    var unitNumber = $(this).closest('.unit').find('.unitName .number input').val();
+                    $(this).closest('.stuffListItem').find('.number input').attr('readonly', 'readonly').val(unitNumber);
+                } else {
+                    $(this).closest('.stuffListItem').find('.number input').removeAttr('readonly');
+                }
+                calculateTotalPoints();
+            })
+            .on('click', '.toggleAmau', function() {
+                $(this).prev().trigger('click');
+                $(this).toggleClass('highlight');
+            })
+            .ready(function() {
+                $('input[type="checkbox"]').each(function() {
+                    if ($(this).attr('checked')) {
+                        $(this).next().addClass('highlight');
+                    }
+                    $(this).hide();
+                });
+            });
+
+        $('.unit .stuffListItem .number')
+            .on('click', '.toZero', function () {
+                $(this).closest('.stuffListItem').find('.toggleAmau.highlight').trigger('click');
+                $(this).prev().val(0);
+                calculateTotalPoints();
+            });
+
+
+        // squad line number change
+        $('.squad_line_new').ready(function(){
+            $('.unit .unitName .number input').each(function () {
+                if ($(this).val()) {
+                    $(this).trigger('change');
+                }
+            });
+        });
+
+        $('.unit .unitName .number')
+            .on('change', 'input', function() {
+                // opacity if < 0
+                var stuffList = $(this).closest('.unit').find('.stuffList');
+                var newNb = $(this).val();
+                if (newNb > 0) {
+                    stuffList.fadeTo('slow', '1');
+                } else {
+                    stuffList.fadeTo('slow', '.2');
+                }
+
+                // value if "as many" selected
+                stuffList.find('.asManyAsUnit input:checked').each(function () {
+                    $(this).closest('.stuffListItem').find('.number input').val(newNb);
+                });
+                    
+
+                return false;
+            });
+
+        $('.unit .unitName .number, .unit .stuffListItem .number')
+            .on('change', 'input[data-points]', function() {
+                    calculateTotalPoints();
+            });
+
+
+        calculateTotalPoints = function() {
+            var total = 0;
+            $('#websiteContent input[data-points]').each(function() {
+                var nb = parseInt($(this).val());
+
+                if (nb > 0) {
+                    total += nb * parseInt($(this).attr('data-points'));
+                }
+            });
+            $('#squadTotal').text(total);
+        }
+    }
+});
