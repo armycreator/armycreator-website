8664aadfbb0f78dcd8e4411edbaa4dcc8dbb299a
Julien Deniau <julien.deniau@gmail.com>
2014-03-17 13:20:08 +0100

    add canchoosenumber limit

---
+++ src/Sitioweb/Bundle/ArmyCreatorBundle/Twig/Squad.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Sitioweb\Bundle\ArmyCreatorBundle\Twig;
+
+use \Doctrine\Common\Persistence\ObjectManager;
+
+use \Sitioweb\Bundle\ArmyCreatorBundle\Entity\SquadLine;
+
+
+class Squad extends \Twig_Extension
+{
+    /**
+     * objectManager
+     *
+     * @var ObjectManager
+     * @access private
+     */
+    private $objectManager;
+
+    /**
+     * __construct
+     *
+     * @param ObjectManager $objectManager
+     * @access public
+     * @return void
+     */
+    public function __construct(ObjectManager $objectManager)
+    {
+        $this->objectManager = $objectManager;
+    }
+
+    /**
+     * getFunctions
+     *
+     * @access public
+     * @return array
+     */
+    public function getFunctions()
+    {
+        return array(
+            'canChooseNumber' => new \Twig_Function_Method($this, 'canChooseNumber')
+        );
+    }
+
+    /**
+     * canChooseNumber
+     *
+     * @param SquadLine $squadLine
+     * @access public
+     * @return string
+     */
+    public function canChooseNumber(SquadLine $squadLine)
+    {
+        $group = $squadLine->getSquad()->getUnitGroup();
+        $unit = $squadLine->getUnit();
+
+        $uhug = $this->objectManager
+            ->getRepository('SitiowebArmyCreatorBundle:UnitHasUnitGroup')
+            ->findOneBy(['unit' => $unit, 'group' => $group]);
+
+        if ($uhug) {
+            return $uhug->getCanChooseNumber();
+        }
+
+        return true;
+    }
+
+    /**
+     * getName
+     *
+     * @access public
+     * @return string
+     */
+    public function getName()
+    {
+        return 'squad';
+    }
+}
+
+
