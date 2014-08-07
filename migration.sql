2ad96e7aad03cbc7ec5586c4fcec5f25370bbd10
Julien Deniau <julien.deniau@gmail.com>
2014-08-07 19:45:55 +0200

    remove migration script (command instead)

--- migration.sql
+++
@@ -1,2 +0,0 @@
-UPDATE Stuff s JOIN Weapon e ON e.id = s.id SET s.description = CONCAT('s:',LENGTH(e.rule), ':"', e.rule, '";');
-UPDATE Stuff s JOIN Equipement e ON e.id = s.id SET s.description = CONCAT('s:',LENGTH(e.description), ':"', e.description, '";');
