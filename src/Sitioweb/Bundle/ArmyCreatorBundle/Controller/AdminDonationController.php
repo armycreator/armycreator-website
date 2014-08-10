fc84068d5a5c400886902e3ad5a1d5ba804b1fbc
Julien Deniau <julien.deniau@gmail.com>
2014-08-10 13:34:49 +0200

    Donation BO

---
+++ src/Sitioweb/Bundle/ArmyCreatorBundle/Controller/AdminDonationController.php
@@ -0,0 +1,233 @@
+<?php
+
+namespace Sitioweb\Bundle\ArmyCreatorBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sitioweb\Bundle\ArmyCreatorBundle\Entity\Donation;
+use Sitioweb\Bundle\ArmyCreatorBundle\Form\DonationType;
+
+/**
+ * Admin donation controller.
+ *
+ * @Route("/admin/donation")
+ */
+class AdminDonationController extends Controller
+{
+
+    /**
+     * Lists all Donation entities.
+     *
+     * @Route("/", name="admin_donation")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('SitiowebArmyCreatorBundle:Donation')
+            ->findBy([], ['createdAt' => 'DESC']);
+
+        $totalByYear = [];
+        foreach ($entities as $donation) {
+            $year = $donation->getYear();
+            if (!isset($totalByYear[$year])) {
+                $totalByYear[$year] = 0;
+            }
+            $totalByYear[$year] += $donation->getAmount();
+        }
+
+        return array(
+            'entities' => $entities,
+            'totalByYear' => $totalByYear,
+        );
+    }
+    /**
+     * Creates a new Donation entity.
+     *
+     * @Route("/", name="admin_donation_create")
+     * @Method("POST")
+     * @Template("SitiowebArmyCreatorBundle:Donation:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Donation();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_donation'));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Creates a form to create a Donation entity.
+     *
+     * @param Donation $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Donation $entity)
+    {
+        $form = $this->createForm(new DonationType(), $entity, array(
+            'action' => $this->generateUrl('admin_donation_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Donation entity.
+     *
+     * @Route("/new", name="admin_donation_new")
+     * @Method("GET")
+     * @Template()
+     */
+    public function newAction()
+    {
+        $entity = new Donation();
+        $form   = $this->createCreateForm($entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Donation entity.
+     *
+     * @Route("/{id}/edit", name="admin_donation_edit")
+     * @Method("GET")
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('SitiowebArmyCreatorBundle:Donation')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Donation entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+    * Creates a form to edit a Donation entity.
+    *
+    * @param Donation $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Donation $entity)
+    {
+        $form = $this->createForm(new DonationType(), $entity, array(
+            'action' => $this->generateUrl('admin_donation_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Donation entity.
+     *
+     * @Route("/{id}", name="admin_donation_update")
+     * @Method("PUT")
+     * @Template("SitiowebArmyCreatorBundle:Donation:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('SitiowebArmyCreatorBundle:Donation')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Donation entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_donation'));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+    /**
+     * Deletes a Donation entity.
+     *
+     * @Route("/{id}", name="admin_donation_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('SitiowebArmyCreatorBundle:Donation')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Donation entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('admin_donation'));
+    }
+
+    /**
+     * Creates a form to delete a Donation entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_donation_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+}
---
+++ src/Sitioweb/Bundle/ArmyCreatorBundle/Entity/Donation.php
@@ -0,0 +1,260 @@
+<?php
+
+namespace Sitioweb\Bundle\ArmyCreatorBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Donation
+ *
+ * @ORM\Table(indexes={@ORM\Index(name="year", columns={"year"})})
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ */
+class Donation
+{
+    /**
+     * @var int $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * createdAt
+     *
+     * @var mixed
+     * @access private
+     *
+     * @ORM\Column(name="createdAt", type="datetime", nullable=false)
+     */
+    private $createdAt;
+
+    /**
+     * year
+     *
+     * @var int
+     * @access private
+     *
+     * @ORM\Column(name="year", type="integer", nullable=false)
+     */
+    private $year;
+
+    /**
+     * email
+     *
+     * @var string
+     * @access private
+     *
+     * @ORM\Column(name="email", type="string", nullable=false)
+     */
+    private $email;
+
+    /**
+     * amount
+     *
+     * @var float
+     * @access private
+     *
+     * @ORM\Column(name="amount", type="float", nullable=false)
+     */
+    private $amount;
+
+    /**
+     * public
+     *
+     * @var boolean
+     * @access private
+     *
+     * @ORM\Column(name="isPublic", type="boolean", nullable=false)
+     */
+    private $public;
+
+    /**
+     * user
+     *
+     * @var User
+     * @access private
+     *
+	 * @ORM\ManyToOne(targetEntity="User")
+     */
+    private $user;
+
+    /**
+     * __construct
+     *
+     * @access public
+     */
+    public function __construct()
+    {
+        $this->public = true;
+    }
+
+    /**
+     * Gets the value of id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Gets the value of createdAt
+     *
+     * @return DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Sets the value of createdAt
+     *
+     * @param DateTime $createdAt created date
+     *
+     * @return Donation
+     */
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    /**
+     * Gets the value of year
+     *
+     * @return int
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * Sets the value of year
+     *
+     * @param int $year year
+     *
+     * @return Donation
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+        return $this;
+    }
+
+    /**
+     * Gets the value of email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Sets the value of email
+     *
+     * @param string $email email
+     *
+     * @return Donation
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * Gets the value of amount
+     *
+     * @return float
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * Sets the value of amount
+     *
+     * @param float $amount amount
+     *
+     * @return Donation
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+        return $this;
+    }
+
+    /**
+     * Gets the value of public
+     *
+     * @return boolean
+     */
+    public function isPublic()
+    {
+        return $this->public;
+    }
+
+    /**
+     * Sets the value of public
+     *
+     * @param boolean $public public
+     *
+     * @return Donation
+     */
+    public function setPublic($public)
+    {
+        $this->public = $public;
+        return $this;
+    }
+
+    /**
+     * Gets the value of user
+     *
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Sets the value of user
+     *
+     * @param User $user user
+     *
+     * @return Donation
+     */
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    /**
+     * prePersist
+     *
+     * @access public
+     * @return void
+     *
+     * @ORM\PrePersist
+     */
+    public function prePersist()
+    {
+        if (!$this->createdAt) {
+            $this->createdAt = new \DateTime;
+        }
+        if (!$this->year) {
+            $this->year = (int) $this->createdAt->format('Y');
+        }
+    }
+}
