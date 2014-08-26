a67c85010a73e8948bf3437439e47359aa8e6aa6
Julien Deniau <julien.deniau@gmail.com>
2014-08-26 12:48:26 +0200

    indentation

--- src/Sitioweb/Bundle/ArmyCreatorBundle/Controller/UnitHasUnitGroupController.php
+++ src/Sitioweb/Bundle/ArmyCreatorBundle/Controller/UnitHasUnitGroupController.php
@@ -15,207 +15,207 @@ use Sitioweb\Bundle\ArmyCreatorBundle\Entity\UnitHasUnitGroup;
 use Sitioweb\Bundle\ArmyCreatorBundle\Form\UnitHasUnitGroupType;
 
 /**
-* UnitHasUnitGroup controller.
-*
-* @Route("/admin/{game}/{breed}/unithasunitgroup")
-* @ParamConverter("game", class="SitiowebArmyCreatorBundle:Game", options={"mapping": {"game" = "code"}})
-* @ParamConverter("breed", class="SitiowebArmyCreatorBundle:Breed", options={"mapping": {"breed" = "slug"}})
-*/
+ * UnitHasUnitGroup controller.
+ *
+ * @Route("/admin/{game}/{breed}/unithasunitgroup")
+ * @ParamConverter("game", class="SitiowebArmyCreatorBundle:Game", options={"mapping": {"game" = "code"}})
+ * @ParamConverter("breed", class="SitiowebArmyCreatorBundle:Breed", options={"mapping": {"breed" = "slug"}})
+ */
 class UnitHasUnitGroupController extends Controller
 {
-/**
- * Creates a new UnitHasUnitGroup entity.
- *
- * @Route("/", name="unithasunitgroup_create")
- * @Method("POST")
- * @Template("SitiowebArmyCreatorBundle:UnitHasUnitGroup:new.html.twig")
- */
-public function createAction(Request $request, Breed $breed)
-{
-    if (!$this->get('oneup_acl.manager')->isGranted('EDIT', $breed)) {
-        throw new AccessDeniedException();
-    }
+    /**
+     * Creates a new UnitHasUnitGroup entity.
+     *
+     * @Route("/", name="unithasunitgroup_create")
+     * @Method("POST")
+     * @Template("SitiowebArmyCreatorBundle:UnitHasUnitGroup:new.html.twig")
+     */
+    public function createAction(Request $request, Breed $breed)
+    {
+        if (!$this->get('oneup_acl.manager')->isGranted('EDIT', $breed)) {
+            throw new AccessDeniedException();
+        }
 
-    $entity  = new UnitHasUnitGroup();
-    $form = $this->createForm(new UnitHasUnitGroupType($breed), $entity);
-    $form->bind($request);
+        $entity  = new UnitHasUnitGroup();
+        $form = $this->createForm(new UnitHasUnitGroupType($breed), $entity);
+        $form->bind($request);
 
-    if ($form->isValid()) {
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($entity);
-        $em->flush();
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
 
-        return $this->redirect($this->getArmyShowUrl($breed));
-    }
+            return $this->redirect($this->getArmyShowUrl($breed));
+        }
 
-    return array(
-        'entity' => $entity,
-        'form'   => $form->createView(),
-    );
-}
-
-/**
- * Displays a form to create a new UnitHasUnitGroup entity.
- *
- * @Route("/new", name="unithasunitgroup_new")
- * @Method("GET")
- * @Template()
- */
-public function newAction(Request $request, Breed $breed)
-{
-    if (!$this->get('oneup_acl.manager')->isGranted('EDIT', $breed)) {
-        throw new AccessDeniedException();
-    }
-
-    $entity = new UnitHasUnitGroup();
-    if ($request->query->has('group')) {
-        $em = $this->getDoctrine()->getManager();
-        $group = $em->getRepository('SitiowebArmyCreatorBundle:UnitGroup')->findOneBy(
-            array(
-                'breed' => $breed,
-                'slug' => $request->query->get('group')
-            )
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
         );
-        $entity->setGroup($group);
     }
 
-    $form   = $this->createForm(new UnitHasUnitGroupType($breed), $entity);
+    /**
+     * Displays a form to create a new UnitHasUnitGroup entity.
+     *
+     * @Route("/new", name="unithasunitgroup_new")
+     * @Method("GET")
+     * @Template()
+     */
+    public function newAction(Request $request, Breed $breed)
+    {
+        if (!$this->get('oneup_acl.manager')->isGranted('EDIT', $breed)) {
+            throw new AccessDeniedException();
+        }
 
-    return array(
-        'entity' => $entity,
-        'form'   => $form->createView(),
-    );
-}
+        $entity = new UnitHasUnitGroup();
+        if ($request->query->has('group')) {
+            $em = $this->getDoctrine()->getManager();
+            $group = $em->getRepository('SitiowebArmyCreatorBundle:UnitGroup')->findOneBy(
+                array(
+                    'breed' => $breed,
+                    'slug' => $request->query->get('group')
+                )
+            );
+            $entity->setGroup($group);
+        }
 
-/**
- * Displays a form to edit an existing UnitHasUnitGroup entity.
- *
- * @Route("/{id}/edit", name="unithasunitgroup_edit")
- * @Method("GET")
- * @Template()
- */
-public function editAction($id, Breed $breed)
-{
-    if (!$this->get('oneup_acl.manager')->isGranted('EDIT', $breed)) {
-        throw new AccessDeniedException();
+        $form   = $this->createForm(new UnitHasUnitGroupType($breed), $entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
     }
 
-    $em = $this->getDoctrine()->getManager();
+    /**
+     * Displays a form to edit an existing UnitHasUnitGroup entity.
+     *
+     * @Route("/{id}/edit", name="unithasunitgroup_edit")
+     * @Method("GET")
+     * @Template()
+     */
+    public function editAction($id, Breed $breed)
+    {
+        if (!$this->get('oneup_acl.manager')->isGranted('EDIT', $breed)) {
+            throw new AccessDeniedException();
+        }
 
-    $entity = $em->getRepository('SitiowebArmyCreatorBundle:UnitHasUnitGroup')->find($id);
-
-    if (!$entity) {
-        throw $this->createNotFoundException('Unable to find UnitHasUnitGroup entity.');
-    }
-
-    $editForm = $this->createForm(new UnitHasUnitGroupType($breed), $entity);
-    $deleteForm = $this->createDeleteForm($id);
-
-    return array(
-        'entity'      => $entity,
-        'edit_form'   => $editForm->createView(),
-        'delete_form' => $deleteForm->createView(),
-    );
-}
-
-/**
- * Edits an existing UnitHasUnitGroup entity.
- *
- * @Route("/{id}/update", name="unithasunitgroup_update")
- * @Template("SitiowebArmyCreatorBundle:UnitHasUnitGroup:edit.html.twig")
- */
-public function updateAction(Breed $breed, Request $request, $id)
-{
-    if (!$this->get('oneup_acl.manager')->isGranted('EDIT', $breed)) {
-        throw new AccessDeniedException();
-    }
-
-    $em = $this->getDoctrine()->getManager();
-
-    $entity = $em->getRepository('SitiowebArmyCreatorBundle:UnitHasUnitGroup')->find($id);
-
-    if (!$entity) {
-        throw $this->createNotFoundException('Unable to find UnitHasUnitGroup entity.');
-    }
-
-    $deleteForm = $this->createDeleteForm($id);
-    $editForm = $this->createForm(new UnitHasUnitGroupType($breed), $entity);
-    $editForm->bind($request);
-
-    if ($editForm->isValid()) {
-        $em->persist($entity);
-        $em->flush();
-
-        return $this->redirect($this->getArmyShowUrl($breed));
-    }
-
-    return array(
-        'entity'      => $entity,
-        'edit_form'   => $editForm->createView(),
-        'delete_form' => $deleteForm->createView(),
-    );
-}
-
-/**
- * Deletes a UnitHasUnitGroup entity.
- *
- * @Route("/{id}/delete", name="unithasunitgroup_delete")
- */
-public function deleteAction(Request $request, $id)
-{
-    if (!$this->get('oneup_acl.manager')->isGranted('EDIT', $breed)) {
-        throw new AccessDeniedException();
-    }
-
-    $form = $this->createDeleteForm($id);
-    $form->bind($request);
-
-    if ($form->isValid()) {
         $em = $this->getDoctrine()->getManager();
+
         $entity = $em->getRepository('SitiowebArmyCreatorBundle:UnitHasUnitGroup')->find($id);
 
         if (!$entity) {
             throw $this->createNotFoundException('Unable to find UnitHasUnitGroup entity.');
         }
 
-        $em->remove($entity);
-        $em->flush();
+        $editForm = $this->createForm(new UnitHasUnitGroupType($breed), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
     }
 
-    return $this->redirect($this->generateUrl('unithasunitgroup'));
-}
+    /**
+     * Edits an existing UnitHasUnitGroup entity.
+     *
+     * @Route("/{id}/update", name="unithasunitgroup_update")
+     * @Template("SitiowebArmyCreatorBundle:UnitHasUnitGroup:edit.html.twig")
+     */
+    public function updateAction(Breed $breed, Request $request, $id)
+    {
+        if (!$this->get('oneup_acl.manager')->isGranted('EDIT', $breed)) {
+            throw new AccessDeniedException();
+        }
 
-/**
- * moveAction
- *
- * @access public
- * @return Response
- *
- * @ParamConverter("unitHasUnitGroup", class="SitiowebArmyCreatorBundle:UnitHasUnitGroup")
- * @Route("/{id}/move_{direction}", name="unithasunitgroup_move")
- * @Template()
- */
-public function moveAction(UnitHasUnitGroup $unitHasUnitGroup, Breed $breed, Request $request, $direction)
-{
-    if (!$this->get('oneup_acl.manager')->isGranted('EDIT', $breed)) {
-        throw new AccessDeniedException();
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('SitiowebArmyCreatorBundle:UnitHasUnitGroup')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find UnitHasUnitGroup entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new UnitHasUnitGroupType($breed), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->getArmyShowUrl($breed));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
     }
 
-    $position = $request->query->get('position');
+    /**
+     * Deletes a UnitHasUnitGroup entity.
+     *
+     * @Route("/{id}/delete", name="unithasunitgroup_delete")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        if (!$this->get('oneup_acl.manager')->isGranted('EDIT', $breed)) {
+            throw new AccessDeniedException();
+        }
 
-    $em = $this->getDoctrine()->getManager();
-    $qb = $em->createQueryBuilder();
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
 
-    if ($direction == 'up') {
-        $posExpr = $qb->expr()->gte('uhug.position', ':position');
-    } else {
-        $posExpr = $qb->expr()->lte('uhug.position', ':position');
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('SitiowebArmyCreatorBundle:UnitHasUnitGroup')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find UnitHasUnitGroup entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('unithasunitgroup'));
     }
 
-    $qb->select('uhug')
-        ->from('SitiowebArmyCreatorBundle:UnitHasUnitGroup', 'uhug')
-        ->where('uhug.group = :unitGroup')
-        ->andWhere(
+    /**
+     * moveAction
+     *
+     * @access public
+     * @return Response
+     *
+     * @ParamConverter("unitHasUnitGroup", class="SitiowebArmyCreatorBundle:UnitHasUnitGroup")
+     * @Route("/{id}/move_{direction}", name="unithasunitgroup_move")
+     * @Template()
+     */
+    public function moveAction(UnitHasUnitGroup $unitHasUnitGroup, Breed $breed, Request $request, $direction)
+    {
+        if (!$this->get('oneup_acl.manager')->isGranted('EDIT', $breed)) {
+            throw new AccessDeniedException();
+        }
+
+        $position = $request->query->get('position');
+
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->createQueryBuilder();
+
+        if ($direction == 'up') {
+            $posExpr = $qb->expr()->gte('uhug.position', ':position');
+        } else {
+            $posExpr = $qb->expr()->lte('uhug.position', ':position');
+        }
+
+        $qb->select('uhug')
+            ->from('SitiowebArmyCreatorBundle:UnitHasUnitGroup', 'uhug')
+            ->where('uhug.group = :unitGroup')
+            ->andWhere(
                 $qb->expr()->orx(
                     $qb->expr()->isNull('uhug.position'),
                     $posExpr
@@ -226,10 +226,10 @@ public function moveAction(UnitHasUnitGroup $unitHasUnitGroup, Breed $breed, Req
             ->setParameters(
                 [
                     'unitGroup' => $unitHasUnitGroup->getGroup(),
-                    'position' => $position,
-                    'uhug' => $unitHasUnitGroup,
-                ]
-        );
+                        'position' => $position,
+                        'uhug' => $unitHasUnitGroup,
+                    ]
+                );
 
         $entityList = $qb->getQuery()->getResult();
 
@@ -264,18 +264,18 @@ public function moveAction(UnitHasUnitGroup $unitHasUnitGroup, Breed $breed, Req
         return $this->createFormBuilder(array('id' => $id))
             ->add('id', 'hidden')
             ->getForm()
-        ;
+            ;
     }
 
     private function getArmyShowUrl(Breed $breed)
     {
         $url = $this->generateUrl(
-                'admin_breed_show',
-                array(
-                    'breed' => $breed->getSlug(),
-                    'game' => $breed->getGame()->getCode()
-                    )
-                );
+            'admin_breed_show',
+            array(
+                'breed' => $breed->getSlug(),
+                'game' => $breed->getGame()->getCode()
+            )
+        );
         return $url;
     }
 }
