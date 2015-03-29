9dedafee273089b034c9334fe08654a7c223e925
Julien Deniau <julien.deniau@gmail.com>
2015-03-29 19:56:49 +0200

    cors ?

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -37,6 +37,7 @@ class AppKernel extends Kernel
             new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
             new Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle(),
 
             new Sitioweb\Bundle\ArmyCreatorBundle\SitiowebArmyCreatorBundle(),
